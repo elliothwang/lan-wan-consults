fb3b96621c7f9745112ddc4801ba82d96dd83a89
elliothwang <elliothwang@gmail.com>
2022-09-20 20:53:49 -0700

    add assets, constants, & tailwindcss

---
+++ src/constants/index.js
@@ -0,0 +1,178 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "services",
+    title: "Services",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const services = [
+  {
+    id: "service-1",
+    icon: star,
+    title: "Technical Assessments",
+    content:
+      "something about tech assessments",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Staffing Solutions",
+    content:
+      "something about staffing solutions",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "LAN/WAN Consulting",
+    content:
+      "something about consulting",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "People Consulted",
+    value: "1000+",
+  },
+  {
+    id: "stats-2",
+    title: "Consulting Period",
+    value: "4-12 months",
+  },
+  {
+    id: "stats-3",
+    title: "Average Salary",
+    value: "$80k+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "",
+      },
+      {
+        name: "How it Works",
+        link: "",
+      },
+      {
+        name: "Create",
+        link: "",
+      },
+      {
+        name: "Explore",
+        link: "",
+      },
+      {
+        name: "Terms & Services",
+        link: "",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "",
+      },
+      {
+        name: "Partners",
+        link: "",
+      },
+      {
+        name: "Suggestions",
+        link: "",
+      },
+      {
+        name: "Blog",
+        link: "",
+      },
+      {
+        name: "Newsletters",
+        link: "",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "",
+      },
+      {
+        name: "Become a Partner",
+        link: "",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
