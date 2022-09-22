6bf2d08e6bb638e6c539cab5d265cb630bd043af
elliothwang <elliothwang@gmail.com>
2022-09-21 18:25:52 -0700

    fix components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import styles from './style';
+import { About, Clients, CTA, Footer, Hero, Navbar, Services, Testimonials } from './components/index';
 
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar />
       </div>
     </div>
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Hero
+        <Hero />
       </div>
     </div>
     <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Services
-        About
-        Clients
-        Testimonials
-        CTA
-        Footer
+        <Services />
+        <About />
+        <Clients />
+        <Testimonials />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>
