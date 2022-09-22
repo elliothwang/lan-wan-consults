ec59f2cc42d4e6b47d5009790d8d15262b3b31fc
elliothwang <elliothwang@gmail.com>
2022-09-21 18:20:17 -0700

    create components index

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './style';
 
+
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
---
+++ src/constants/components/index.js
@@ -0,0 +1,23 @@
+import About from "./About";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Services from "./Services";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import GetStarted from "./GetStarted";
+
+export {
+  About,
+  Clients,
+  CTA,
+  Footer,
+  Hero,
+  Navbar,
+  Services,
+  Stats,
+  Testimonials,
+  GetStarted,
+};
